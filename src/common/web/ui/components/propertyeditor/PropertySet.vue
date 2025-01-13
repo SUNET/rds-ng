69762b6226191a742c0ecdb641221bde52954bd9
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2025-01-13 17:35:11 +0100

    WIP: adds search and profile filter to PE

--- src/common/web/ui/components/propertyeditor/PropertySet.vue
+++ src/common/web/ui/components/propertyeditor/PropertySet.vue
@@ -3,6 +3,9 @@ import Button from "primevue/button";
 import Chip from "primevue/chip";
 import Dialog from "primevue/dialog";
 import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import InputSwitch from "primevue/inputswitch";
 import InputText from "primevue/inputtext";
 import OrderList from "primevue/orderlist";
 
@@ -12,16 +15,31 @@ import { calculateLayout as makeLayout } from "./utils/PropertyEditorUtils";
 
 import { PropertyObjectStore } from "./PropertyObjectStore";
 import PropertyOneCol from "./PropertyOneCol.vue";
-import { ProfileLayoutClass } from "./PropertyProfile";
+import { type ProfileID, ProfileLayoutClass } from "./PropertyProfile";
 
 const props = defineProps(["controller", "project", "projectProfiles", "propertyObjects", "sharedPropertyObjectStore"]);
 const colorsStore = useColorsStore();
 
 var layout = makeLayout(props.projectProfiles);
 
-const propsToShow = ref<ProfileLayoutClass[]>(
+const profileFilter = ref<ProfileID[]>([]);
+const requiredOnly = ref(false);
+const addSearchString = ref("");
+const searchString = ref("");
+
+// TODO Comment and maybe refactor to have dynamic filters
+const propsToShow = computed(() =>
     layout
-        .filter((e: ProfileLayoutClass) => e.required || props.propertyObjects.get(e.id) !== undefined)
+        .filter((e: ProfileLayoutClass) => e.required || (requiredOnly.value ? false : props.propertyObjects.get(e.id) !== undefined))
+        .filter(
+            (e: ProfileLayoutClass) =>
+                profileFilter.value.length === 0 || e.profiles.some((p: ProfileID) => profileFilter.value.map((i) => i[0]).includes(p[0]))
+        )
+        .filter(
+            (e: ProfileLayoutClass) =>
+                e.getDisplayLabel().toLowerCase().includes(searchString.value.toLowerCase()) ||
+                e.description?.toLowerCase().includes(searchString.value.toLowerCase())
+        )
         .sort((a: ProfileLayoutClass, b: ProfileLayoutClass) => -a.profiles!.length - -b.profiles!.length)
 );
 
@@ -29,6 +47,7 @@ const selectedProperties = ref([]) as Ref<ProfileLayoutClass[]>;
 const unselectProperties = () => (selectedProperties.value = []);
 const selectProperties = (selection: ProfileLayoutClass[]) => (selectedProperties.value = selection);
 
+// FIXME
 const hideProperty = (id: string) => {
     propsToShow.value = propsToShow.value.filter((e: ProfileLayoutClass) => e.getId() != id);
 };
@@ -41,16 +60,68 @@ const hiddenPropertys = computed(() =>
 const filteredProperties = computed(() =>
     hiddenPropertys.value.filter(
         (e: ProfileLayoutClass) =>
-            e.getDisplayLabel().toLowerCase().includes(searchString.value.toLowerCase()) ||
-            e.description?.toLowerCase().includes(searchString.value.toLowerCase())
+            e.getDisplayLabel().toLowerCase().includes(addSearchString.value.toLowerCase()) ||
+            e.description?.toLowerCase().includes(addSearchString.value.toLowerCase())
     )
 );
 
-const searchString = ref("");
+const resetFilters = () => {
+    profileFilter.value = [];
+    searchString.value = "";
+    requiredOnly.value = false;
+};
 </script>
 
 <template>
     <div class="w-full max-w-full">
+        <!-- TODO In eigene Komponente auslagern, auch zum "add Properties" Dialog hinzufuegen. -->
+        <div class="m-5 mb-5 mr-0" :class="projectProfiles.list().length > 1 ? 'border-b' : ''">
+            <IconField iconPosition="left">
+                <InputIcon class="pi pi-search"> </InputIcon>
+                <InputText type="text" v-model="searchString" id="searchString" class="w-full" placeholder="Search..." autocomplete="off" />
+            </IconField>
+
+            <div v-if="projectProfiles.list().length > 1" class="my-3 flex justify-between">
+                <span class="flex align-center gap-2">
+                    <Chip
+                        :label="`All (${layout.filter((e: ProfileLayoutClass) => e.required || props.propertyObjects.get(e.id) !== undefined).length})`"
+                        title="Show all properties"
+                        class="h-4 !rounded py-3 text-sm border border-slate-700 cursor-pointer"
+                        :class="profileFilter.length === 0 && !searchString && !requiredOnly ? 'text-emerald-100 bg-slate-700' : ''"
+                        @click="resetFilters"
+                    />
+                    <Chip
+                        v-for="profile in projectProfiles.list()"
+                        :label="profile.getDisplayLabel()"
+                        class="h-4 !rounded py-3 text-sm border cursor-pointer"
+                        :class="profileFilter.includes(profile.getId()) ? 'bg-emerald-50 border-emerald-600 text-slate-700' : ''"
+                        @click="
+                            profileFilter.includes(profile.getId())
+                                ? (profileFilter = profileFilter.filter((e) => e !== profile.getId()))
+                                : profileFilter.push(profile.getId())
+                        "
+                    />
+                </span>
+                <span class="flex align-center gap-2" title="Hide optional properties." aria-label="Hide optional properties.">
+                    <label for="required">Required only</label>
+                    <InputSwitch v-model="requiredOnly" inputId="required" />
+                </span>
+            </div>
+        </div>
+
+        <!-- TODO: Put into its own component -->
+        <div v-if="searchString && propsToShow.length == 0" class="w-full text-center">
+            No matches for <span class="font-bold">{{ searchString }}</span
+            >...
+        </div>
+        <div v-else-if="searchString">
+            <div class="w-full text-center">
+                <span class="font-bold">{{ propsToShow.length }}</span> match{{ propsToShow.length > 1 ? `es` : "" }} for
+                <span class="font-bold">{{ searchString }}</span
+                >...
+            </div>
+        </div>
+
         <PropertyOneCol
             v-for="(p, i) in propsToShow"
             :key="p.id"
@@ -70,7 +141,9 @@ const searchString = ref("");
         icon="material-icons-outlined mi-add"
         size="large"
         rounded
+        severity="primary"
         @click="showAddProperties = true"
+        v-tooltip="{ value: 'Add more properties' }"
     />
 
     <Dialog
@@ -81,13 +154,13 @@ const searchString = ref("");
         :style="{ width: '50vw', height: '80vh' }"
         @after-hide="
             unselectProperties();
-            searchString = '';
+            addSearchString = '';
         "
     >
         <template #default>
             <div class="h-full flex-col flex space-y-4">
                 <FloatLabel>
-                    <InputText type="text" v-model="searchString" id="searchString" class="w-full" />
+                    <InputText type="text" v-model="addSearchString" id="searchString" class="w-full" />
                     <label for="searchString">Search...</label>
                 </FloatLabel>
                 <OrderList
@@ -122,7 +195,7 @@ const searchString = ref("");
                     @click="
                         propsToShow.push(...selectedProperties);
                         unselectProperties();
-                        searchString = '';
+                        addSearchString = '';
                         showAddProperties = false;
                     "
                     >Add {{ selectedProperties.length ? "(" + selectedProperties.length + ")" : "" }}
@@ -132,7 +205,7 @@ const searchString = ref("");
                     severity="secondary"
                     @click="
                         unselectProperties();
-                        searchString = '';
+                        addSearchString = '';
                         showAddProperties = false;
                     "
                     >Cancel
