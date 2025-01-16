e9a3da019d36cc5414f79364cc8fbcaeb2935635
Daniel Müller <daniel.mueller@uni-muenster.de>
2025-01-16 12:34:19 +0100

    make connector targets configurable

--- src/connectors/base/settings/__init__.py
+++ src/connectors/base/settings/__init__.py
@@ -2,5 +2,6 @@ from .authorization_setting_ids import (
     AuthorizationSettingIDs,
     OAuth2AuthorizationSettingIDs,
 )
+from .connector_setting_ids import ConnectorSettingIDs
 from .connector_settings import get_connector_settings
 from .transmission_setting_ids import TransmissionSettingIDs
