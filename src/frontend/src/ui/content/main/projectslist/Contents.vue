7144abc84bcf2955fccefad996a8aab1af3cab06
Daniel Müller <daniel.mueller@uni-muenster.de>
2025-06-06 13:40:05 +0200

    more color changes

--- src/frontend/src/ui/content/main/projectslist/Contents.vue
+++ src/frontend/src/ui/content/main/projectslist/Contents.vue
@@ -4,7 +4,7 @@ import ProjectsListbox from "@/ui/content/main/projectslist/ProjectsListbox.vue"
 </script>
 
 <template>
-    <div class="grid grid-rows-[1fr_4.25rem] grid-cols-1 bg-surface-50">
+    <div class="grid grid-rows-[1fr_4.25rem] grid-cols-1 bg-surface-50 border-r border-[var(--p-surface-200)]">
         <ProjectsListbox />
         <NewProject />
     </div>
