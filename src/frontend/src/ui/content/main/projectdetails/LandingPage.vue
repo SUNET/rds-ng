96f2ecfd0788ae419823a2bf232d355c741c5155
Daniel Müller <daniel.mueller@uni-muenster.de>
2025-07-01 13:55:46 +0200

    landing page redesign

--- src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
+++ src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
@@ -9,7 +9,39 @@ const comp = FrontendComponent.inject();
 </script>
 
 <template>
-    <div>
+    <div class="grid grid-cols-[auto_1fr] gap-20 r-shade-text pt-5">
+        <div class="flex flex-col items-center gap-y-2 text-sm">
+            <img src="@assets/img/rds_ng-octopus-blue.png" alt="logo" class="w-[30rem]" />
+            <Card class="w-[22rem] bg-primary-100 text-primary !p-0" :pt="{ title: '!text-base', body: 'p-3 px-5' }">
+                <template #title>
+                    <div class="flex items-center gap-1">
+                        <span class="material-icons-outlined mi-construction !text-xl" />
+                        <span class="">This is a beta version!</span>
+                    </div>
+                </template>
+                <template #content>
+                    <span class="text-sm">
+                        If you encounter any issues or have other kinds of feedback, feel free to
+                        <ExternalLink link="mailto:sciebo.rds@uni-muenster.de" text="send us an email" />
+                        !
+                    </span>
+                </template>
+            </Card>
+        </div>
+        <div class="flex flex-col max-w-[35rem]">
+            <div class="text-4xl font-bold mt-10">Hello, I'm {{ comp.data.title }}!</div>
+            <div class="text-xl font-bold">Your assistent for easy and secure research data sharing.</div>
+            <div>&nbsp;</div>
+            <div>I'm here to help you manage, prepare, and share your research data - flexibly, securely, and in line with your workflows.</div>
+            <div>My goal: making your data handling easier and safe, so you can stay focused on your research.</div>
+            <div>&nbsp;</div>
+            <div class="grid justify-items-center">
+                <img src="@assets/img/rds-steps-web.png" class="h-full max-h-[calc(100vh-30rem)]" alt="Steps" />
+            </div>
+        </div>
+    </div>
+
+    <!--<div>
         <div class="r-shade-text m-10 !mb-0 !pb-0">
             <div class="grid grid-cols-2 min-w-full min-h-full place-content-start gap-24 pb-0">
                 <div class="space-y-4 !pb-0">
@@ -80,7 +112,7 @@ const comp = FrontendComponent.inject();
                 </div>
             </div>
         </div>
-    </div>
+    </div>-->
 </template>
 
 <style scoped lang="scss">
