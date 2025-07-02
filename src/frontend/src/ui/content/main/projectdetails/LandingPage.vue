8b74b5af8f9f94bc30b3486630f380695e75940e
Daniel Müller <daniel.mueller@uni-muenster.de>
2025-07-02 17:57:59 +0200

    landing page work

--- src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
+++ src/frontend/src/ui/content/main/projectdetails/LandingPage.vue
@@ -9,10 +9,17 @@ const comp = FrontendComponent.inject();
 </script>
 
 <template>
-    <div class="grid grid-cols-[auto_1fr] gap-20 r-shade-text pt-5">
+    <div class="grid grid-cols-[auto_1fr] gap-x-20 gap-y-0 r-shade-text px-5">
         <div class="flex flex-col items-center gap-y-2 text-sm">
             <img src="@assets/img/rds_ng-octopus-blue.png" alt="logo" class="w-[30rem]" />
-            <Card class="w-[22rem] bg-primary-100 text-primary !p-0" :pt="{ title: '!text-base', body: 'p-3 px-5' }">
+        </div>
+        <div class="flex flex-col">
+            <div class="text-4xl font-bold mt-10">Hello, I'm {{ comp.data.title }}!</div>
+            <div class="text-xl font-bold">Your assistent for easy and secure research data sharing.</div>
+            <div>&nbsp;</div>
+            <div>I'm here to help you manage, prepare, and share your research data - flexibly, securely, and in line with your workflows.</div>
+            <div>My goal: making your data handling easier and safe, so you can stay focused on your research.</div>
+            <Card class="bg-primary-100 text-primary !p-0 mt-4" :pt="{ title: '!text-base', caption: 'h-5', body: 'p-3 px-5' }">
                 <template #title>
                     <div class="flex items-center gap-1">
                         <span class="material-icons-outlined mi-construction !text-xl" />
@@ -28,16 +35,8 @@ const comp = FrontendComponent.inject();
                 </template>
             </Card>
         </div>
-        <div class="flex flex-col max-w-[35rem]">
-            <div class="text-4xl font-bold mt-10">Hello, I'm {{ comp.data.title }}!</div>
-            <div class="text-xl font-bold">Your assistent for easy and secure research data sharing.</div>
-            <div>&nbsp;</div>
-            <div>I'm here to help you manage, prepare, and share your research data - flexibly, securely, and in line with your workflows.</div>
-            <div>My goal: making your data handling easier and safe, so you can stay focused on your research.</div>
-            <div>&nbsp;</div>
-            <div>
-                <img src="@assets/img/landing-steps.png" class="w-full h-full pr-8" alt="Steps" />
-            </div>
+        <div class="col-span-2">
+            <img src="@assets/img/landing-steps.png" class="w-[100rem] h-full pr-2" alt="Steps" />
         </div>
     </div>
 </template>
