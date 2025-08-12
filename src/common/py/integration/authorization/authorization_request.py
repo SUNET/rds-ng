648d4514f083b643dcec25555c1e970ead3d2e22
Daniel Müller <daniel.mueller@uni-muenster.de>
2025-08-12 12:56:04 +0200

    missing docs

--- src/common/py/integration/authorization/authorization_request.py
+++ src/common/py/integration/authorization/authorization_request.py
@@ -15,6 +15,7 @@ class AuthorizationRequestPayload:
         auth_id: The authorization ID.
         auth_type: The authorization type.
         auth_issuer: The entity that requires the authorization.
+        auth_issuer_url: The URL of above entity.
         auth_bearer: The entity that will be authorized against.
 
         fingerprint: The user's fingerprint.
